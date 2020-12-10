ec429b85e8e6929159dd410afede07a3879d6173
Bharath <bharath.balagopal@gmail.com>
2020-12-10 00:47:48 -0500

    the commit with that hacky implementation, im lazy tho

--- pull_data.py
+++ pull_data.py
@@ -20,13 +20,22 @@ def get_teamnames(league_id, season, week, swid, espn_long):
         cookies={"SWID": swid, "espn_s2": espn_long}
     ).json()
 
-
     team_names = {tm['id']: tm['location'].strip() + ' ' + tm['nickname'].strip() for tm in raw['teams']}
 
     return team_names
 
+#this feels like a really hacky implemetation im not happy with this :( note to sober be fix this
+def process_teams(teams):
+    list_of_teams = []
+
+    for i, j in teams.items():
+        list_of_teams.append(j)
+    
+    return list_of_teams
+
+
 def main():
-    #this is hard coding the position codes, used for creating the lineups
+    #this is hard coding the defaults change them here or in the creds setup further down
     pos_codes = {
         0 : 'QB', 1 : 'QB',
         2 : 'RB', 3 : 'RB',
@@ -38,7 +47,9 @@ def main():
         21: 'IR',
         23: 'Flex'
     }  
-    
+    positions = ['QB', 'RB', 'WR', 'Flex', 'TE', 'D/ST', 'K']
+    structure = [1, 2, 2, 1, 1, 1, 1]
+
     #loading/ creating all the necessary info
     try:
         #load cached info
@@ -46,7 +57,7 @@ def main():
             creds = json.load(j)
     except:
         #no cached file found? Do things...
-        print("You need a creds file first lets make that for next time! \nConsult README.md for what these are and the formats")
+        print("You need a creds file!!! Lets make that for next time. \nConsult README.md for what these are and the formats")
         league_id = input("League ID: ")
         positions = input('''Input your positions (default: '['QB', 'RB', 'WR', 'Flex', 'TE', 'D/ST', 'K']'): ''')
         structure = input('''Input your sturcture (default '[1, 2, 2, 1, 1, 1, 1]'): ''')
@@ -68,7 +79,15 @@ def main():
     season = int(input("Season (year eg. 2020): "))
     week = int(input("Week (number eg. 11): "))
 
+    for i in range(1, week+1):
+        week = i
+        raw_pull = get_raw(creds['league_id'], season, week, creds['swid'], creds['espn_long'])
+        team_names = get_teamnames(creds['league_id'], season, week, creds['swid'], creds['espn_long'])
+        teams = process_teams(team_names)
     
+    print(teams)
+
+
 
 main()
 
